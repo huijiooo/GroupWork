6db79ab7431be14086b42fbd3a329243eae88489
HuijingGONG <huijing.gong@student.unimelb.edu.au>
2024-05-31 20:30:15 +1000

    start to write js

---
+++ html/script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    var menuBox = document.querySelector('header .header_left');
+    var cartLink1 = document.querySelector('header a.cart1');
+    var cartLink2 = document.querySelector('header a.cart2');
+    var shoppingCart = document.querySelector('shoppingCart');
+    var menu_hezi_box = document.querySelector('menu_hezi_box');
+})
