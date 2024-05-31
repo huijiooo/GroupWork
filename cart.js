3fbd4ede4e2e0e482528c6bf756c1bb2228ce871
Xinyuew14 <161696892+Xinyuew14@users.noreply.github.com>
2024-05-31 23:03:09 +1000

    Add product to cart function js

---
+++ cart.js
@@ -0,0 +1,7 @@
+//Gets the cart list element
+var cartList = document.getElementById('cart-list');
+let success_shop = document.querySelector('.success_shop');
+//Get the success_shop element
+//A message indicating that an item has been successfully added to the cart
+
+
