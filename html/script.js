d30d8ed9b7c57d5844f16e8f3446318164c3be88
HuijingGONG <huijing.gong@student.unimelb.edu.au>
2024-05-31 20:48:03 +1000

    Click to show shopping cart part

--- html/script.js
+++ html/script.js
@@ -3,6 +3,13 @@ document.addEventListener("DOMContentLoaded", function () {
     var menuBox = document.querySelector('header .header_left');
     var cartLink1 = document.querySelector('header a.cart1');
     var cartLink2 = document.querySelector('header a.cart2');
-    var shoppingCart = document.querySelector('shoppingCart');
-    var menu_hezi_box = document.querySelector('menu_hezi_box');
+    var shoppingCart = document.querySelector('.shoppingCart');
+    var menu_hezi_box = document.querySelector('.menu_hezi_box');
+
+    // Click to show shopping cart
+    cartLink1.addEventListener('click', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        shoppingCart.style.display = 'block';
+    });
 })
