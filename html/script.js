e0b42b87574734ce78a5988a9fa46cc431a11cfa
HuijingGONG <huijing.gong@student.unimelb.edu.au>
2024-05-31 21:04:23 +1000

    Click elsewhere to hide the cart + add some comment to help after check

--- html/script.js
+++ html/script.js
@@ -12,4 +12,29 @@ document.addEventListener("DOMContentLoaded", function () {
         event.stopPropagation();
         shoppingCart.style.display = 'block';
     });
+
+    cartLink2.addEventListener('click', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        shoppingCart.style.display = 'block';
+    });
+    // Click elsewhere to hide the cart
+    document.addEventListener('click', function (event) {
+        var isClickInsideCart = shoppingCart.contains(event.target);
+        var isClickInsideCart2 = menu_hezi_box.contains(event.target);
+        var isClickInsideCartIcon = cartLink1.contains(event.target);
+        var isClickInsideCartIcon = cartLink2.contains(event.target);
+    
+        if (!isClickInsideCart && !isClickInsideCartIcon && !isClickInsideCart2) {
+            shoppingCart.style.display = 'none';
+            menu_hezi_box.style.display = 'none';
+        }
+    });
+    
+    menuBox.addEventListener('click', function (event) {
+        console.log(7272);
+        event.preventDefault();
+        event.stopPropagation();
+        menu_hezi_box.style.display = 'block';
+    });
 })
