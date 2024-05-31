c57ee9f5b6088e5cdd3f99a437e3e7d095ac4d85
HuijingGONG <huijing.gong@student.unimelb.edu.au>
2024-05-31 21:38:15 +1000

    finish the js+ change misspelling（fitter->filter）

--- html/script.js
+++ html/script.js
@@ -37,4 +37,89 @@ document.addEventListener("DOMContentLoaded", function () {
         event.stopPropagation();
         menu_hezi_box.style.display = 'block';
     });
+
+    ///////////////////
+// Shortcut search term
+var searchHistory = ['Cranberry', 'Honey', 'seafood', 'chill', 'BBQ'];
+
+//search input box
+//Use focus to help run the following script if the user's focus is on the search bar
+document.getElementById('searchInput').addEventListener('focus', function () {
+    var datalist = document.getElementById('searchHistory');
+    datalist.innerHTML = '';
+
+    searchHistory.forEach(function (item) {
+        var option = document.createElement('option');
+        option.value = item;
+        datalist.appendChild(option);
+    });
+});
+document.getElementById('searchInput_mobile').addEventListener('focus', function () {
+    var datalist = document.getElementById('searchHistory_mobile');
+    datalist.innerHTML = '';
+
+    searchHistory.forEach(function (item) {
+        var option = document.createElement('option');
+        option.value = item;
+        datalist.appendChild(option);
+    });
+});
+// document.getElementById('searchInput').addEventListener('input', function () {
+//     var datalist = document.getElementById('searchHistory');
+//     datalist.innerHTML = '';
+
+//     searchHistory.forEach(function (item) {
+//         if (item.includes(this.value)) {
+//             var option = document.createElement('option');
+//             option.value = item;
+//             datalist.appendChild(option);
+//         }
+//     }, this);
+// });
+
+// Search button click events
+document.querySelector('.search button').addEventListener('click', function () {//Execute when the button is clicked
+    var input = document.getElementById('searchInput');//Gets the input box element with id searchInput.
+    var searchTerm = input.value.trim();//Delete the Spaces before and after it
+
+    //Check that the search term is valid (not empty) and not in the search history.
+    if (searchTerm && !searchHistory.includes(searchTerm)) {
+        searchHistory.unshift(searchTerm);
+        //If the number of records in the search history exceeds 10, the oldest record (i.e. the last record) is deleted.
+        if (searchHistory.length > 10) {
+            searchHistory.pop();
+        }
+        //Clear the search box so that the user can enter a new search term the next time.
+        var datalist = document.getElementById('searchHistory');
+        datalist.innerHTML = '';
+        searchHistory.forEach(function (item) {
+            var option = document.createElement('option');
+            option.value = item;
+            datalist.appendChild(option);
+        });
+
+        input.value = '';
+    }
+});
+
+
+var anniu1 = document.querySelector('.mobile_box .anniu1');
+var anniu2 = document.querySelector('.mobile_box .anniu2');
+var anniu3 = document.querySelector('.mobile_box .anniu3');
+var thisNavs = document.querySelectorAll('.mobile_box .this_nav');
+anniu1.addEventListener('click', function () {
+    thisNavs[0].style.display = (thisNavs[0].style.display === 'block') ? 'none' : 'block';
+    thisNavs[1].style.display = 'none';
+    thisNavs[2].style.display = 'none';
+});
+anniu2.addEventListener('click', function () {
+    thisNavs[0].style.display = 'none';
+    thisNavs[1].style.display = 'block';
+    thisNavs[2].style.display = 'none';
+});
+anniu3.addEventListener('click', function () {
+    thisNavs[0].style.display = 'none';
+    thisNavs[1].style.display = 'none';
+    thisNavs[2].style.display = 'block';
+});
 })
